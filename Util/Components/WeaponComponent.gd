aca96e0d6e4e8abc9c867288a7a78dfb12129364
Obli <obliotter@gmail.com>
2025-01-22 23:53:02 -0600

    Update WeaponComponent.gd

--- Util/Components/WeaponComponent.gd
+++ Util/Components/WeaponComponent.gd
@@ -16,6 +16,6 @@ func show_weapon(value):
 	showing_weapon = value
 	weapon_sprite.visible = value
 
-# TODO: ROTATE THE PLAYER.
+# TODO: ROTATE CHARACTER TOWARDS PLAYER TO FOLLOW THE MOUSE. No upside down gun.
 func rotate_weapon(direction: Vector2):
 	weapon_sprite.look_at(direction)
