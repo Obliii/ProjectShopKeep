a089f464ff6f06702e8cf47bbdda24f98e0b19bb
Funce <funce.973@gmail.com>
2025-01-23 22:37:45 +1300

    Added Health Component Check for Hitbox Component

--- Util/Components/HitboxComponent.gd
+++ Util/Components/HitboxComponent.gd
@@ -5,6 +5,9 @@ class_name HitboxComponent
 
 # Detects if the hitbox has been touched by an attack component and deals damage accordingly.
 func _on_body_entered(body) -> void:
+	if !health_component:
+		return
+		
 	var attack: AttackComponent
 	for component in body.get_children():
 		if component is AttackComponent:
