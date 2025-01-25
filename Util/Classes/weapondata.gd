444239059cb549224c60660fefa494738e4da7f7
Funce <funce.973@gmail.com>
2025-01-25 16:02:51 +1300

    Weapon Data Configuration - 3 weapon data resources defined

--- Util/Classes/weapondata.gd
+++ Util/Classes/weapondata.gd
@@ -1,5 +1,17 @@
 extends Resource
 class_name WeaponData
 
-@export var damage: int
-@export var armor_piercing: int
+# Weapon Texture
+@export var texture: Texture2D
+
+#Delay in between attacks
+@export var attack_speed: int
+
+# Bullet information - damage, ap
+@export var bullet: BulletData
+
+# Exclusively for the AI module to figure out the optimal range.
+@export var ai_attack_range: int
+
+
+# Any "bonuses" or adjustments to the bullet should be here
