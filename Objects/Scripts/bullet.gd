9bd16ec0bed0472303fa893d81e13c7c26c21794
Funce <funce.973@gmail.com>
2025-01-26 23:28:28 +1300

    plus one more

--- Objects/Scripts/bullet.gd
+++ Objects/Scripts/bullet.gd
@@ -7,8 +7,11 @@ const BULLET_TIMEOUT: int = 60
 
 @onready var move_component: MoveComponent = $MoveComponent
 @onready var damage_component: DamageComponent = $DamageComponent
+@onready var hurtbox_component: HurtboxComponent = $HurtboxComponent
 
 @export var bullet_data : BulletData
+
+var shooter: PhysicsBody2D
 
 
 var direction: Vector2
@@ -21,6 +24,8 @@ func _ready() -> void:
 	move_component.reset_speed()
 	
 	sprite_2d.texture = bullet_data.texture
+	
+	hurtbox_component.entity_blacklist.append(shooter)
 	
 	# Remove yourself after the bullet timeout elapses
 	get_tree().create_timer(BULLET_TIMEOUT).timeout.connect(queue_free)
