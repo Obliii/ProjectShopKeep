bf39c977026a90a6027f31246f519e68f4ff72d3
Funce <funce.973@gmail.com>
2025-01-26 20:52:28 +1300

    This one is a monster, bullets fire, hit walls and disappear. Base 'Rocket Launcher' has been set, sprite now flips based on mouse movement and bullets dont collide with the player in regards to movement

--- Util/Classes/weapondata.gd
+++ Util/Classes/weapondata.gd
@@ -4,14 +4,16 @@ class_name WeaponData
 # Weapon Texture
 @export var texture: Texture2D
 
-#Delay in between attacks
-@export var attack_speed: int
+#Delay in between attacks in seconds
+@export var attack_speed: float
 
 # Bullet information - damage, ap
 @export var bullet_data: BulletData
 
 # Exclusively for the AI module to figure out the optimal range.
 @export var ai_attack_range: int
+
+@export var bullet_offset: Vector2
 
 
 # Any "bonuses" or adjustments to the bullet should be here
--- Util/Components/MoveComponent.gd
+++ Util/Components/MoveComponent.gd
@@ -11,13 +11,6 @@ class_name MoveComponent
 
 var speed: int
 var can_dash: bool = true
-# This is a workaround due to not being able to use the sprite.flip_h since it won't affect animations.
-# It will flip the character ONE TIME, and won't do a flip loop if you try to move LEFT. ... yes. It does that.
-var flip: bool = false:
-	set(value):
-		if(flip != value):
-			entity.scale.x *= -1
-			flip = value
 
 func _ready():
 	reset_speed()
@@ -28,12 +21,6 @@ func reset_speed() -> void:
 
 # Moves the player and flips the sprite.
 func move(vector: Vector2, _delta):
-	#If there's a sprite, then the sprite will flip depending on where its moving.
-	# I'm not using sprite's flip_h because I want to also flip the animation.
-	if vector.x > 0:
-		flip = false
-	elif vector.x < 0:
-		flip = true
 	entity.move_and_collide(vector * speed * _delta)
 
 # Fires the start_dashing function if the entity can dash.
