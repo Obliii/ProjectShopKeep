8e8fab5902dfc461de3f9f34e89320b39341fa70
Funce <funce.973@gmail.com>
2025-02-01 02:12:34 +1300

    Loose enemy pathfinding

--- Util/Components/AIComponent.gd
+++ Util/Components/AIComponent.gd
@@ -3,13 +3,21 @@ class_name AIComponent
 
 @export var move_component: MoveComponent
 @export var weapon_component: WeaponComponent
+@export var navigation_agent_2d: NavigationAgent2D
+
 
 @onready var enemy: Enemy = get_owner()
+
+
+
 
 var player: Player
 
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("player")
+
+func _process(_delta: float) -> void:
+	navigation_agent_2d.target_position = player.global_position
 
 func _physics_process(delta: float) -> void:
 	move_character(delta)
@@ -18,7 +26,7 @@ func _physics_process(delta: float) -> void:
 
 func move_character(delta):
 	if !ai_in_range():
-		var vec: Vector2 = player_direction()
+		var vec: Vector2 = enemy.global_position.direction_to(navigation_agent_2d.get_next_path_position())
 		move_component.move(vec, delta)
 
 func fire_primary_weapon():
@@ -37,7 +45,8 @@ func dash():
 		move_component.dash()
 
 func ai_in_range() -> bool:
-	return enemy.position.distance_to(player.position) < weapon_component.weapon_data.ai_attack_range
+	return false
+	#return enemy.position.distance_to(player.position) < weapon_component.weapon_data.ai_attack_range
 
 func player_direction() -> Vector2:
 	return (player.global_position - enemy.global_position).normalized()
