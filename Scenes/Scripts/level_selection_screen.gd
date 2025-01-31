65ede376a0df17640a49239b76cea018d2d74c8f
Obli <obliotter@gmail.com>
2025-01-31 02:28:12 -0600

    Imports Kobold Texture and adds it in for Prototype build

--- Scenes/Scripts/level_selection_screen.gd
+++ Scenes/Scripts/level_selection_screen.gd
@@ -5,12 +5,14 @@ extends CanvasLayer
 
 
 func _on_kobold_cafe_button_pressed() -> void:
-	var map = kobold_motel_map.instantiate()
-	game_instance.gameplay_screen.add_child(map)
-	game_instance.hud_screen.setup_screen()
-	Main.player_character.get_node("HealthComponent").reset_health()
-	await game_instance.switch_screen(game_instance.gameplay_screen, true)
-	game_instance.close_all_ui()
+	if !Main.game_playing:
+		Main.game_playing = true
+		var map = kobold_motel_map.instantiate()
+		game_instance.gameplay_screen.add_child(map)
+		game_instance.hud_screen.setup_screen()
+		Main.player_character.get_node("HealthComponent").reset_health()
+		await game_instance.switch_screen(game_instance.gameplay_screen, true)
+		game_instance.close_all_ui()
 
 func _on_back_button_pressed() -> void:
 	game_instance.switch_screen(game_instance.title_screen)
